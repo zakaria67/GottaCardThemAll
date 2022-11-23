b0a6cc4cf95b21d9dba4bac8993d0fb326fbcc20
zakaria67 <salsabile-mp@hotmail.com>
2022-11-23 16:00:33 +0100

    Initial commit

---
+++ src/be/intecbrussel/Friend.java
@@ -0,0 +1,13 @@
+public class Friend {
+    private String name;
+    private boolean haveCard;
+    private int number1;
+    private int number2;
+
+    public Friend(String name, boolean haveCard, int number1, int number2) {
+        this.name = name;
+        this.haveCard = haveCard;
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+}
